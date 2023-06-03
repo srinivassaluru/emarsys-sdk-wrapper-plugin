acff06a419358eb5c237d0ff538d45067bba19ee
Zen <Zen.Huzaini@gds.ey.com>
2023-06-03 12:24:09 +0200

    add android fixes

--- android/src/main/java/com/lla/emarsyssdk/EmarsysSDKCustomPlugin.java
+++ android/src/main/java/com/lla/emarsyssdk/EmarsysSDKCustomPlugin.java
@@ -2,6 +2,7 @@ package com.lla.emarsyssdk;
 
 import android.util.Log;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.getcapacitor.JSObject;
 import com.getcapacitor.Plugin;
 import com.getcapacitor.PluginCall;
@@ -13,10 +14,13 @@ import com.emarsys.config.EmarsysConfig;
 
 import com.emarsys.inapp.ui.InlineInAppView;
 
+import java.io.IOException;
+import java.util.Map;
+
 @CapacitorPlugin(name = "EmarsysSDKCustom")
 public class EmarsysSDKCustomPlugin extends Plugin {
     public EmarsysConfig config;
-
+    public String emarrsysDeviceInformationConfig;
     private EmarsysSDKCustom implementation = new EmarsysSDKCustom();
     EmarsysPushNotification emarsysPushNotification = new EmarsysPushNotification();
 
@@ -68,16 +72,39 @@ public class EmarsysSDKCustomPlugin extends Plugin {
     @PluginMethod
     public void setUser(PluginCall call){
         String value = call.getString("value");
-        System.out.println("SET USER IN THE PLUGIN " + value);
-
         emarsysPushNotification.setContactUser(value);
     }
 
     @PluginMethod
     public void clearUser(PluginCall call){
         String value = call.getString("value");
-        System.out.println("CLEAR USER IN THE PLUGIN " + value);
-
         emarsysPushNotification.clearContactUser();
+    }
+
+    @PluginMethod
+    public void getDeviceInformation(PluginCall call) {
+        String value = this.emarrsysDeviceInformationConfig;
+
+        JSObject ret = new JSObject();
+        ret.put("value", implementation.echo(value));
+        call.resolve(ret);
+    }
+
+    @PluginMethod
+    public void trackEvent(PluginCall call) {
+        String eventName = call.getString("eventName");
+        String eventAttributes = call.getString("eventAttributes");
+        ObjectMapper mapper = new ObjectMapper();
+
+        try{
+            Map<String, String> mappedEventAtributes = mapper.readValue(eventAttributes, Map.class);
+            JSObject ret = new JSObject();
+            assert eventName != null;
+            Emarsys.trackCustomEvent(eventName, mappedEventAtributes);
+
+            call.resolve(ret);
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
